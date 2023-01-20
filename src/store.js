15beb5f0314e0d49c3d96b8dc3ef4f232f0d40f0
SURAJIT BERA <109613164+surajitbera123@users.noreply.github.com>
2023-01-20 18:48:44 +0530

    Delete store.js

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import StudentReducer from "./features/StudentSlice";
-
-export default configureStore({
-    reducer:{
-        student: StudentReducer
-    }
-})
